b558fb784d8ca33d48148db79b49e1af404a2a41
Carter Stanton <carter.stanton@humanit.govcloud>
2022-06-08 12:32:55 -0400

    Created Navbar component.
    This allows for site wide navigation.

---
+++ components/navbar.js
@@ -0,0 +1,25 @@
+import Link from 'next/link';
+import Image from 'next/image';
+import styles from './styles/navbar.module.css';
+
+export default function Navbar() {
+  return (
+    <nav role='navigation' className={styles.Navbar} aria-label='Main'>
+        <Link href='/' passHref>
+          <a className={styles.NavLink}>
+            <Image
+              src="/centerville-barbers-logo.png"
+              height={53}
+              width={200}
+              alt="Centerville Barbers"
+            />
+          </a>
+        </Link>
+      <Link href='/' passHref>
+        <a className={styles.NavLink}>
+          Home
+        </a>
+      </Link>
+    </nav>
+  )
+}
