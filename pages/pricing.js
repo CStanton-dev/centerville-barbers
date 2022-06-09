ada96e4a605d3a455db9369f6d033cf0d3c7f9fd
Carter Stanton <carter.stanton@humanit.govcloud>
2022-06-09 09:46:53 -0400

    Added Pricing Page.
    This page displays all the prices for the given products and haircuts offered from the barbershop.

---
+++ pages/pricing.js
@@ -0,0 +1,28 @@
+import Head from 'next/head';
+import Layout from '../components/layout';
+import PricingCard from '../components/pricingCard';
+import styles from '../styles/Pricing.module.css';
+
+export default function Pricing() {
+  const pricedItems = [
+    {id: 1,title: 'Haircut', price: 15, includes: ['Haircut', 'Back of Neck Shave', 'Hot Towel Head Wrap'], status: 'available'},
+    {id: 2, title: 'Face Shave', price: 25, includes: ['Full Face Shave (razor)', 'Hot Towel Head Wrap'], status: 'unavailable'},
+    {id: 3, title: 'Head Shave', price: 15, includes: ['Full Head Shave (razor)', 'Hot Towel Head Wrap'], status: 'available'},
+    {id: 4, title: 'Beard Trim', price: 5, includes: ['Beard Trim (no razor)'], status: 'unavailable'},
+    {id: 5, title: 'Facial', price: 10, includes: ['Rolling Cream', 'Cold Cream', 'Facial Massage'], status: 'available'},
+    {id: 6, title: 'Package', price: 45, includes: ['Haircut', 'Face Shave', 'Facial'], status: 'available'},
+  ]
+
+  return (
+    <Layout>
+      <Head>
+        <title>Pricing</title>
+      </Head>
+      <h1 className={styles.title}>Pricing</h1>
+      <p className={styles.description}>Here is a pricing list of our haircuts, beard trims, and more.</p>
+      <section className={styles.grid}>
+        <PricingCard items={pricedItems}/>
+      </section>
+    </Layout>
+  )
+}
