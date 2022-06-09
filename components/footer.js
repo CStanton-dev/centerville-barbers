e31c1b321bd172b223a52e2a1c6d33b6848583c0
Carter Stanton <carter.stanton@humanit.govcloud>
2022-06-09 14:38:50 -0400

    Created Footer Component.
    This component contains links to the Privacy Policy Page, the Meet The Team Page, and external links to Facebook and Yelp.

---
+++ components/footer.js
@@ -0,0 +1,49 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import styles from './styles/footer.module.css';
+
+export default function Footer() {
+  return (
+    <footer role='footer' className={styles.footer}>
+      <div className={styles.copyright}>
+        <Image
+          src='/centerville-barbers-logomark.png'
+          width={24}
+          height={24}
+          alt='Logomark'
+        />
+        &nbsp;&copy;{' '}2022
+      </div>
+      <div>
+        <h3 className={styles.listCategory}>About</h3>
+        <ul className={styles.list}>
+          <li>
+            <Link href='/meet-the-team' passHref>
+              <a>Meet The Team</a>
+            </Link>
+          </li>
+          <li>  
+            <Link href='https://www.facebook.com/CentervilleBarbers/'>
+              <a target="_blank" rel="noopener noreferrer">Facebook</a>
+            </Link>
+          </li>
+          <li>  
+            <Link href='https://www.yelp.com/biz/centerville-barbers-a-little-off-the-top-centerville-3'>
+              <a target="_blank" rel="noopener noreferrer">Yelp</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h3 className={styles.listCategory}>Legal</h3>
+        <ul className={styles.list}>
+          <li>
+            <Link href='/privacy-policy' passHref>
+              <a>Privacy Policy</a>
+            </Link>
+          </li>
+        </ul>
+      </div>
+    </footer>
+  )
+}
